397c80c3f75c6040a73954f4e61aabbe6fe60323
Yeremia395 <53548038+Yeremia395@users.noreply.github.com>
2019-10-09 18:51:50 +0700

    first commit

---
+++ src/pbo/P2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo;
+
+/**
+ *
+ * @author Yeremia Rizky
+ */
+public class P2 {
+    public static void main(String[] args){
+        
+        System.out.println("Hello");
+        System.out.println("Selamat belajar program java");
+        
+    }
+    
+}
---
+++ src/pbo/P3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo;
+
+/**
+ *
+ * @author Yeremia Rizky
+ */
+public class P3 {
+    public static void main(String[] args){
+        
+        int nilai_pbo, nilai_web;
+        
+        nilai_pbo = 90;
+        nilai_web = 80;
+        
+        System.out.println("Variable yang terdapat pada program: ");
+        System.out.println("Nilai PBO " +nilai_pbo);
+        System.out.println("Nilai WEB " +nilai_web);
+    }
+    
+}
